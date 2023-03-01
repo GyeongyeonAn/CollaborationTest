90cf813ccac6e57b74a73ac9f01d6cacd07cb38f
agy_97 <55589616+GyeongyeonAn@users.noreply.github.com>
2023-03-01 18:59:45 +0900

    feat: Cube 생성

---
+++ Assets/Scripts/Cube_BranchNumber.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_BranchNumber : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("Cube 생성 완료 : 안경연");
+    }
+}
